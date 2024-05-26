eaa730a07b8ccad1080d428dcaf8348a69526eed
unknown <vermaanurag550@gmail.com>
2024-05-26 07:09:05 +0530

    first commit

--- src/Components/ImageBlock.jsx
+++ src/Components/ImageBlock.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { LazyLoadImage } from 'react-lazy-load-image-component';
 
 const ImageBlock = ({ src }) => {
     
@@ -12,8 +13,8 @@ const ImageBlock = ({ src }) => {
     }
     return (
         <>
-            <div className='lg:w-[300px] border-4 border-black  rounded flex flex-col justify-around'>
-                <div className='h-'><img src={src} /></div>
+            <div className='lg:w-[300px] border-4 border-black w-[280px] rounded flex flex-col justify-around'>
+                <div className='h-'><LazyLoadImage alt="image" src={src} effect="opacity" placeholderSrc={src}/></div>
                 <div className='flex justify-between my-4 items-center text-black px-5'>
                     PicImage
                     <h2 onClick={handleDownload}className='cursor-pointer font-bold rounded border-2 border-black px-2'>
